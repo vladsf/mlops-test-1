8cd1db535225265483520c14ca87e4e80fc0c5fe
Vlad Safronov <thevlad@gmail.com>
2022-12-02 18:23:34 +0500

    Initial import

---
+++ hello.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+def add(x, y):
+    """This is an add function"""
+    
+    return x + y
+
+print(add(1, 1))
+
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
---
+++ test_hello.py
@@ -0,0 +1,6 @@
+from hello import add
+
+def test_add():
+    assert 2 == add(1, 1)
+
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
